3cc23dcf5dbcc1c6bb0712627c7cf9095f41b982
Christopher Swinton <chris@theswintons.co.uk>
2022-01-31 16:53:46 +0000

    Added most recent Icons and New Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,9 +13,10 @@
  import "./layout.css"
  import "./icon.css"
  import "./font.css"
- import Sidebar from "./sidebar"
- import Link from "./link"
  import Footer from "./footer"
+ import Sidebar2 from "./sidebar2"
+ import NavLink2 from "./navlink2"
+ import SubNavLink from "./sublink"
  
  const Layout = ({ children }) => {
    const data = useStaticQuery(graphql`
@@ -31,22 +32,119 @@
    return (
      <div className="layout">
        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-       <Sidebar
+       <Sidebar2
          link1={
-           <Link
-             state="active"
-             linkstate="active-link"
-             icon="key"
-             linkname="Log In"
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="home"
+             linkname="Welcome"
              link="/"
            />
          }
          link2={
-           <Link
+           <NavLink2
              state=""
              linkstate=""
-             icon="user"
-             linkname="Activate Account"
+             icon="dashboard"
+             linkname="Overview"
+             link="404"
+           />
+         }
+         link3={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="mobile"
+             linkname="Phones"
+             link="404"
+           />
+         }
+         link4={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="currency-notes-coins"
+             linkname="Charges"
+             link="404"
+           />
+         }
+         link5={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="landline"
+             linkname="Calls"
+             link="404"
+           />
+         }
+         link6={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="international"
+             linkname="Destinations"
+             link="404"
+           />
+         }
+         link7={
+           <div>
+             <NavLink2
+               state="active2"
+               linkstate="active-link2"
+               icon="file"
+               linkname="Reports"
+               link="/"
+             />
+             <ul className="Sublink-Ul">
+               <SubNavLink
+                 state="active2"
+                 linkstate="active-link2"
+                 linkname="Unbilled"
+                 link="/"
+               />
+               <SubNavLink
+                 state=""
+                 linkstate=""
+                 linkname="Billed"
+                 link="/"
+               />
+             </ul>
+           </div>
+         }
+         link8={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="tag"
+             linkname="Tagging"
+             link="404"
+           />
+         }
+         link9={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="user-admin"
+             linkname="Admin"
+             link="404"
+           />
+         }
+         link10={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="download"
+             linkname="Downloads"
+             link="404"
+           />
+         }
+         link11={
+           <NavLink2
+             state=""
+             linkstate=""
+             icon="time"
+             linkname="Real Time"
              link="404"
            />
          }
---
+++ src/components/sidebar2.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Sidebar2 = props => (
+  <nav className="Sidebar">
+    <ul className="Siderbar-Ul2">
+        {props.link1}
+        {props.link2}
+        {props.link3}
+        {props.link4}
+        {props.link5}
+        {props.link6}
+        {props.link7}
+        {props.link8}
+        {props.link9}
+        {props.link10}
+        {props.link11}
+    </ul>
+  </nav>
+)
+
+export default Sidebar2
